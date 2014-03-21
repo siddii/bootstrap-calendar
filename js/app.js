2e8fbc26f3141071098aede677621c8b02e279c8
Siddique Hameed <siddii@gmail.com>
2014-03-21 11:08:39 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,29 +15,35 @@ $(function() {
 		fillYears();
 		$selectCalendarModal.modal();
 	});
-	
-	$notesCalendar.on('click', 'td.day', function() {
-		var clickedCell = $(this);
-		console.log('####### clickedCell = ', clickedCell);
-		$editNotesModal.data('clicked-cell', clickedCell);
-		var day = clickedCell.find('span').html();
-		var title = 'Notes for '
-				+ calendar.getMonthString() + ' ' + day + ', ' + calendar.getYear();
-		var note = clickedCell.find('.note');
-		$editNotesModal
-				.find('textarea#notes')
-				.val(
-						note.length > 0 && note.html().length > 0 ? replaceHTMLEntities(note
-								.html())
-								: "");
-		$editNotesModal.find('.modal-title').html(title);
-		$editNotesModal.modal();
-	});	
 
-	$notesCalendar.on('render.bs.calendar', function(event, monthContainer, calendarInstance) {
+	$notesCalendar
+			.on(
+					'click',
+					'td.day',
+					function() {
+						var clickedCell = $(this);
+						console.log('####### clickedCell = ', clickedCell);
+						$editNotesModal.data('clicked-cell', clickedCell);
+						var day = clickedCell.find('span').html();
+						var title = 'Notes for ' + calendar.getMonthString()
+								+ ' ' + day + ', ' + calendar.getYear();
+						var note = clickedCell.find('.note');
+						$editNotesModal
+								.find('textarea#notes')
+								.val(
+										note.length > 0
+												&& note.html().length > 0 ? replaceHTMLEntities(note
+												.html())
+												: "");
+						$editNotesModal.find('.modal-title').html(title);
+						$editNotesModal.modal();
+					});
+
+	$notesCalendar.on('render.bs.calendar', function(event, monthContainer,
+			calendarInstance) {
 		calendar = calendar || calendarInstance;
 		console.log('Calendar rendered', monthContainer);
-		loadNotes(monthContainer);	
+		loadNotes(monthContainer);
 	});
 
 	$('#changeCalendar').click(function() {
@@ -45,22 +51,24 @@ $(function() {
 		calendar.setCalendarDate(dateStr).render();
 		$selectCalendarModal.modal('hide');
 	});
-	
-	$('#saveNotes').click(function () { 
+
+	$('#saveNotes').click(function() {
 		var clickedCell = $editNotesModal.data('clicked-cell');
 		var noteContent = $editNotesModal.find('textarea#notes').val().trim();
 		saveNotes(clickedCell.data('date'), noteContent);
 		updateNotes(clickedCell, noteContent);
-		$editNotesModal.modal('hide'); 
+		$editNotesModal.modal('hide');
 	});
 
-	$('.clear-notes').click(function () {
-		$editNotesModal.find('textarea#notes').val(''); 
+	$('.clear-notes').click(function() {
+		$editNotesModal.find('textarea#notes').val('');
 	});
-	
+
 	function updateNotes(clickedCell, noteContent) {
 		clickedCell.find('div.note-wrapper').remove();
-		clickedCell.append('<div class="note-wrapper"><textarea class="note form-control">' + noteContent + "</textarea></div>");
+		clickedCell
+				.append('<div class="note-wrapper"><textarea class="note form-control">'
+						+ noteContent + "</textarea></div>");
 	}
 
 	function fillMonths() {
@@ -85,10 +93,6 @@ $(function() {
 		}
 	}
 
-	function notesModal() {
-		var clickedCell = $(this);
-	}
-
 	var entityReplacementChars = {
 		"nbsp" : " ",
 		"amp" : "&",
@@ -104,46 +108,29 @@ $(function() {
 				}));
 	}
 
+	function formatDate() {
+		var dd = calDate.getDate().toString();
+		var yyyy = calDate.getFullYear().toString();
+		var mm = (calDate.getMonth() + 1).toString();
+		var ret = yyyy + '-' + (mm[1] ? mm : "0" + mm[0]) + '-'
+				+ (dd[1] ? dd : "0" + dd[0]);
+		return ret;
+	}
+
 	function loadNotes(monthContainer) {
-		var notes = localStorage
-				&& localStorage['notes']
-				&& localStorage['notes'][(calDate.getMonth() + 1) + '-'
-						+ calDate.getFullYear()];
-		var days = monthContainer.find('td.day');
-		for ( var i = 0; notes && i < notes.length; i++) {
-			days.each(function(i, dayNode) {
-				if ($(dayNode).data('date') === $(note).data('date')) {
-					$(dayNode).find('.note').remove();
-					$(dayNode).append(note);
-					return false;
+		console.log('Date = ', formatDate());
+		var notes = localStorage && localStorage['notes'] && JSON.parse(localStorage['notes']);
+		if (notes) {
+			monthContainer.find('td.day').each(function(i, dayNode) {
+				var date = $(dayNode).data('date');
+				if (date && notes[date]) {
+					updateNotes($(dayNode), notes[date]);
 				}
-			});
+			});			
 		}
 	}
 
 	function saveNotes(dateStr, content) {
-		var notesObj = localStorage['notes'] || {};
-		notesObj[dateStr] = content;
-		localStorage['notes'] = notesObj;
+		var notes = localStorage['notes'] && JSON.stringify(localStorage['notes']);
 	}
-	
-	/*
-	 * 
-	 * calendar = new Calendar($('#current-month'), calDate, launchNotesModal,
-	 * loadNotes); showCurrentMonthCaption();
-	 * 
-	 * $('.prev-month').click(function () { calendar.scrollMonth(-1); });
-	 * 
-	 * $('.next-month').click(function () { calendar.scrollMonth(1); });
-	 * 
-	 * $('.current-month').click(function () { $('#selectDateModal').modal();
-	 * });
-	 * 
-	 * $('#changeDate').click(function () { calDate = new Date($selYear.val(),
-	 * $selMonth.val(), 1); $('#selectDateModal').modal('hide'); var
-	 * $currentMonthContainer = $('#' + $('.item.active').attr('id')); calendar =
-	 * new Calendar($currentMonthContainer, calDate, launchNotesModal,
-	 * loadNotes); showCurrentMonthCaption(); });
-	 * 
-	 */
 });
